9ede3dc7ec2061da27a42ed1a3c10b97fa5cc52d
Valerii Kuzmych <kuzmychvalerii@gmail.com>
2024-03-21 20:15:02 +0100

    added forms validation

--- SpringFormValidation/spring-formvalidation/src/main/java/com/kuzmych/spring/controllers/PeopleController.java
+++ SpringFormValidation/spring-formvalidation/src/main/java/com/kuzmych/spring/controllers/PeopleController.java
@@ -1,63 +1,75 @@
 package com.kuzmych.spring.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import com.kuzmych.spring.dao.PersonDAO;
 import com.kuzmych.spring.models.Person;
 
-
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
-    private final PersonDAO personDAO;
+	private final PersonDAO personDAO;
 
-    @Autowired
-    public PeopleController(PersonDAO personDAO) {
-        this.personDAO = personDAO;
-    }
+	@Autowired
+	public PeopleController(PersonDAO personDAO) {
+		this.personDAO = personDAO;
+	}
 
-    @GetMapping()
-    public String index(Model model) {
-        model.addAttribute("people", personDAO.index());
-        return "people/index";
-    }
+	@GetMapping()
+	public String index(Model model) {
+		model.addAttribute("people", personDAO.index());
+		return "people/index";
+	}
 
-    @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model) {
-        model.addAttribute("person", personDAO.show(id));
-        return "people/show";
-    }
+	@GetMapping("/{id}")
+	public String show(@PathVariable("id") int id, Model model) {
+		model.addAttribute("person", personDAO.show(id));
+		return "people/show";
+	}
 
-    @GetMapping("/new")
-    public String newPerson(@ModelAttribute("person") Person person) {
-        return "people/new";
-    }
+	@GetMapping("/new")
+	public String newPerson(@ModelAttribute("person") Person person) {
+		return "people/new";
+	}
 
-    @PostMapping("/new")
-    public String create(@ModelAttribute("person") Person person) {
-        personDAO.save(person);
-        return "redirect:/people";
-    }
+	@PostMapping("/new")
+	public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
 
-    @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") int id) {
-        model.addAttribute("person", personDAO.show(id));
-        return "people/edit";
-    }
+		if (bindingResult.hasErrors()) {
+			return "people/new";
+		}
+		personDAO.save(person);
+		return "redirect:/people";
+	}
 
-    @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
-        personDAO.update(id, person);
-        return "redirect:/people";
-    }
+	@GetMapping("/{id}/edit")
+	public String edit(Model model, @PathVariable("id") int id) {
+		model.addAttribute("person", personDAO.show(id));
+		return "people/edit";
+	}
 
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
-        personDAO.delete(id);
-        return "redirect:/people";
-    }
+	@PatchMapping("/{id}")
+	public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+			@PathVariable("id") int id) {
+
+		if (bindingResult.hasErrors()) {
+			return "people/edit";
+		}
+
+		personDAO.update(id, person);
+		return "redirect:/people";
+	}
+
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable("id") int id) {
+		personDAO.delete(id);
+		return "redirect:/people";
+	}
 }
--- SpringFormValidation/spring-formvalidation/src/main/java/com/kuzmych/spring/models/Person.java
+++ SpringFormValidation/spring-formvalidation/src/main/java/com/kuzmych/spring/models/Person.java
@@ -1,11 +1,22 @@
 package com.kuzmych.spring.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
 	
 	
     private int id;
+    
+    @Min(value = 18, message = "Age should be greater tha 18")
     private int age;
+    @NotEmpty(message = "Email should be not empty")
+    @Email(message = "Email should be valid")
     private String email;
+    @NotEmpty(message = "Name should not be empty" )
+    @Size(min = 2, max = 30, message = "Name shuld be between 2 and 30 characters" )
     private String name;
     
    
