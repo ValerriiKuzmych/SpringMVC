cf319e83d30f05c9b48b6129b10502031feeb2b6
Valerii Kuzmych <kuzmychvalerii@gmail.com>
2024-03-24 14:52:34 +0100

    Spring + JDBC API + database MySQL

--- SpringFormValidation/spring-formvalidation/src/main/java/com/kuzmych/spring/dao/PersonDAO.java
+++ SpringFormValidation/spring-formvalidation/src/main/java/com/kuzmych/spring/dao/PersonDAO.java
@@ -4,60 +4,86 @@ import org.springframework.stereotype.Component;
 
 import com.kuzmych.spring.models.Person;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-
 
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
-    private List<Person> people;
 
-    {
-        people = new ArrayList<>();
+	private static final String URL = "jdbc:mysql://localhost:3306/SpringJDBCAPI";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "root";
 
-        people.add(new Person(++PEOPLE_COUNT, 32, "tom@gmail.com", "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, 24, "bob@gmail.com", "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, 29, "mike@gmail.com", "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, 28, "katy@gmail.com", "Katy"));
-    }
+	private static Connection connection;
 
-    public List<Person> index() {
-        return people;
-    }
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 
-    public Person show(int id) {
-        for (Person person : people) {
-            if (person.getId() == id) {
-                return person;
-            }
-        }
-        return null;
-    }
+		try {
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		}
+	}
 
-    public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
-    }
+	public List<Person> index() {
+		List<Person> persons = new ArrayList<>();
 
-    public void update(int id, Person updatedPerson) {
-        Person personToBeUpdated = show(id);
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM Persons");
 
-        personToBeUpdated.setName(updatedPerson.getName());
-        personToBeUpdated.setAge(updatedPerson.getAge());
-        personToBeUpdated.setEmail(updatedPerson.getEmail());
-        
-    }
+			while (resultSet.next()) {
+				Person person = new Person();
+				person.setId(resultSet.getInt("id"));
+				person.setAge(resultSet.getInt("age"));
+				person.setName(resultSet.getString("name"));
+				person.setEmail(resultSet.getString("email"));
+				persons.add(person);
+			}
 
-    public void delete(int id) {
-        Iterator<Person> iterator = people.iterator();
-        while (iterator.hasNext()) {
-            Person person = iterator.next();
-            if (person.getId() == id) {
-                iterator.remove();
-                break; 
-            }
-        }
-    }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return persons;
+
+	}
+
+	/*
+	 * public Person show(int id) { for (Person person : people) { if
+	 * (person.getId() == id) { return person; } } return null; }
+	 */
+
+	public void save(Person person) {
+		
+		
+	}
+
+	/*
+	 * public void update(int id, Person updatedPerson) { Person personToBeUpdated =
+	 * show(id);
+	 * 
+	 * personToBeUpdated.setName(updatedPerson.getName());
+	 * personToBeUpdated.setAge(updatedPerson.getAge());
+	 * personToBeUpdated.setEmail(updatedPerson.getEmail());
+	 * 
+	 * }
+	 */
+
+	/*
+	 * public void delete(int id) { Iterator<Person> iterator = people.iterator();
+	 * while (iterator.hasNext()) { Person person = iterator.next(); if
+	 * (person.getId() == id) { iterator.remove(); break; } } }
+	 */
 }
