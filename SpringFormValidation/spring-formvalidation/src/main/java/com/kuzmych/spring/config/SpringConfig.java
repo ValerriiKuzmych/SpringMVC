9d0ad845c603683dfe544ddc0c4b332e64284158
Valerii Kuzmych <kuzmychvalerii@gmail.com>
2024-03-27 19:26:16 +0100

    JDBC Template

--- SpringFormValidation/spring-formvalidation/src/main/java/com/kuzmych/spring/config/SpringConfig.java
+++ SpringFormValidation/spring-formvalidation/src/main/java/com/kuzmych/spring/config/SpringConfig.java
@@ -1,10 +1,14 @@
 package com.kuzmych.spring.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,40 +16,60 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
-
 @Configuration
 @ComponentScan("{com.kuzmych.spring.config, com.kuzmych.spring.controllers, com.kuzmych.spring.dao, com.kuzmych.spring.models}")
 @EnableWebMvc
 public class SpringConfig implements WebMvcConfigurer {
 
-    private final ApplicationContext applicationContext;
+	private final ApplicationContext applicationContext;
 
-    @Autowired
-    public SpringConfig(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
+	@Autowired
+	public SpringConfig(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
 
-    @Bean
-    public SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("/WEB-INF/views/");
-        templateResolver.setSuffix(".html");
-        return templateResolver;
-    }
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setApplicationContext(applicationContext);
+		templateResolver.setPrefix("/WEB-INF/views/");
+		templateResolver.setSuffix(".html");
+		return templateResolver;
+	}
 
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-        templateEngine.setEnableSpringELCompiler(true);
-        return templateEngine;
-    }
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		templateEngine.setEnableSpringELCompiler(true);
+		return templateEngine;
+	}
 
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-        resolver.setTemplateEngine(templateEngine());
-        registry.viewResolver(resolver);
-    }
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		registry.viewResolver(resolver);
+	}
+
+	@Bean
+	public DataSource dataSource() {
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/SpringJDBCTemplate");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+
+		return dataSource;
+
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+
+		return new JdbcTemplate(dataSource());
+
+	}
 }
--- SpringFormValidation/spring-formvalidation/src/main/java/com/kuzmych/spring/dao/PersonDAO.java
+++ SpringFormValidation/spring-formvalidation/src/main/java/com/kuzmych/spring/dao/PersonDAO.java
@@ -1,151 +1,52 @@
 package com.kuzmych.spring.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import com.kuzmych.spring.models.Person;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
 
-	private static final String URL = "jdbc:mysql://localhost:3306/SpringJDBCAPI";
-	private static final String USERNAME = "root";
-	private static final String PASSWORD = "root";
+	private final JdbcTemplate jdbcTemplate;
 
-	private static Connection connection;
+	@Autowired
+	public PersonDAO(JdbcTemplate jdbcTemplate) {
 
-	static {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-		} catch (SQLException throwables) {
-			throwables.printStackTrace();
-		}
+		this.jdbcTemplate = jdbcTemplate;
 	}
 
 	public List<Person> index() {
-		List<Person> persons = new ArrayList<>();
 
-		try {
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM Persons");
-
-			while (resultSet.next()) {
-				Person person = new Person();
-				person.setId(resultSet.getInt("id"));
-				person.setAge(resultSet.getInt("age"));
-				person.setName(resultSet.getString("name"));
-				person.setEmail(resultSet.getString("email"));
-				persons.add(person);
-			}
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return persons;
+		return jdbcTemplate.query("SELECT * FROM Persons", new BeanPropertyRowMapper<>(Person.class));
 
 	}
 
 	public Person show(int id) {
 
-		Person person = null;
-
-		PreparedStatement preparedStatement;
-
-		try {
-
-			preparedStatement = connection.prepareStatement("SELECT * FROM Persons WHERE id = ?");
-
-			preparedStatement.setInt(1, id);
-
-			ResultSet resultSet = preparedStatement.executeQuery();
-
-			resultSet.next();
-
-			person = new Person();
-
-			person.setId(resultSet.getInt("id"));
-			person.setAge(resultSet.getInt("age"));
-			person.setName(resultSet.getString("name"));
-			person.setEmail(resultSet.getString("email"));
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return person;
+		return jdbcTemplate.query("SELECT * FROM Persons WHERE id=?", new Object[] { id },
+				new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);
 
 	}
 
 	public void save(Person person) {
 
-		PreparedStatement preparedStatement;
-		try {
-			preparedStatement = connection.prepareStatement("INSERT INTO Persons VALUES (?, ?, ?, ?)");
-
-			preparedStatement.setInt(2, person.getAge());
-			preparedStatement.setString(3, person.getName());
-			preparedStatement.setString(4, person.getEmail());
-
-			preparedStatement.executeUpdate();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		jdbcTemplate.update("INSERT INTO Persons VALUES (1, ?, ?, ?)", person.getAge(), person.getName(), person.getEmail());
 
 	}
 
 	public void update(int id, Person updatedPerson) {
 
-		PreparedStatement preparedStatement;
-
-		try {
-
-			preparedStatement = connection.prepareStatement("UPDATE Persons SET age=?, name=?, email=? WHERE id=?");
-
-			preparedStatement.setInt(1, updatedPerson.getAge());
-			preparedStatement.setString(2, updatedPerson.getName());
-			preparedStatement.setString(3, updatedPerson.getEmail());
-			preparedStatement.setInt(4, updatedPerson.getId());
-
-			preparedStatement.executeUpdate();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		jdbcTemplate.update("UPDATE Persons SET age=?, name=?, email=? WHERE id=?", updatedPerson.getAge(), updatedPerson.getName(), updatedPerson.getEmail(), id );
 
 	}
 
 	public void delete(int id) {
-		PreparedStatement preparedStatement;
-
-		try {
-
-			preparedStatement = connection.prepareStatement("DELETE FROM Persons WHERE id=?");
-
-			preparedStatement.setInt(1, id);
-
-			preparedStatement.executeUpdate();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		jdbcTemplate.update("DELETE FROM Persons WHERE id=?", id);
 
 	}
 
